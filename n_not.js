a49b780e0dc37bb5ebe7277dfb753e2156f59917
tanabesan <152053837+tanabesan@users.noreply.github.com>
2024-01-13 10:54:42 +0900

    Update n_not.js

--- n_not.js
+++ n_not.js
@@ -1,9 +1,2 @@
-let notice = Number(document.getElementsByClassName('notice clearfix')[0].getAttribute('id').replace('notice_', ''));
-let lnotice = Number(prompt('フレンド申請は何件来ていますか？'));
-for(let id = notice; id <= notice + lnotice; id++) {
-let nid = 'notice_' + id;
-let target0 = document.getElementById(nid);
-let uid0 = target0.children[2].getAttribute('data-uid');
-let imgNo0 = target0.children[2].getAttribute('data-img_no');
-open_pvm(uid0, imgNo0);
-};
+alert("notice機能は現在調整中です。\n使いたい場合は次のコードをブックマークに入れて使用して下さい。");
+alert("javascript:let notice=Number(document.getElementsByClassName('notice clearfix')[0].getAttribute('id').replace('notice_',''));let lnotice=Number(prompt('フレンド申請は何件来ていますか？'));for(let id=notice;id<=notice+lnotice;id++){let nid='notice_'+id;let target0=document.getElementById(nid);let uid0=target0.children[2].getAttribute('data-uid');let imgNo0=target0.children[2].getAttribute('data-img_no');open_pvm(uid0,imgNo0)}");
